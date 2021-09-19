605c560a77821d636833f32d3d2a6a83f72867ba
Shiva <sh_esnaashari@yahoo.com>
2021-09-18 19:11:57 -0700

    Data-api

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 import requests
 import json 
+from django.http import HttpResponse
+
 key = '1SXC5NCVKRJPOEG6'
 ticker = 'AAPL'
 url = 'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol={}&apikey={}'.format(ticker, key)
