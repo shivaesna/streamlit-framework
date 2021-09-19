b221f1f747cbecdebc820d5d102aa3e5dd2c5028
Shiva <sh_esnaashari@yahoo.com>
2021-09-18 18:53:33 -0700

    Data-api

---
+++ app.py
@@ -0,0 +1,7 @@
+import requests
+import json 
+key = '1SXC5NCVKRJPOEG6'
+ticker = 'AAPL'
+url = 'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol={}&apikey={}'.format(ticker, key)
+response = requests.get(url)
+print(response.json()) 
