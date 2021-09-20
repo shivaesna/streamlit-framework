e8c499b0555ca88ebcefa474af4e85da9f487ec2
Shiva <sh_esnaashari@yahoo.com>
2021-09-19 23:38:32 -0700

    Data-api

--- app.py
+++ app.py
@@ -6,5 +6,7 @@ key = '1SXC5NCVKRJPOEG6'
 ticker = 'AAPL'
 url = 'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol={}&apikey={}'.format(ticker, key)
 response = requests.get(url)
+data = response.json()
 print(response.json()) 
-HttpResponse('<pre>' + 'response.json()' + '</pre>')
+
+#HttpResponse('<pre>' + 'response.json()' + '</pre>')
